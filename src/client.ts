cf469308207a5c4e4ddaffa0bfa75fa092be2e79
Milos Havlicek <miloshavlicek@gmail.com>
2025-03-12 18:28:19 +0100

    feat: Created AOSInit script

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@
 
 export * from "./components/molecules/Legal/CookiesConsentDialog/CookiesConsentDialog";
 export * from "./components/molecules/Recommendations/RecommendationsSwiper/RecommendationsSwiper";
+export * from "./components/scripts/AOSInit";
 export * from "./components/scripts/GoogleAnalytics/GoogleAnalyticsConsent";
 
 // Import CSS styles
