d7cf3e406a22be38f4d45d32b0f1a8cc6e4d83f2
Milos Havlicek <miloshavlicek@gmail.com>
2025-03-13 14:30:41 +0100

    feat: Created FacebookPixel script component

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,7 @@ export * from "./components/molecules/Legal/CookiesConsentDialog/CookiesConsentD
 export * from "./components/molecules/Recommendations/RecommendationsSwiper/RecommendationsSwiper";
 export * from "./components/scripts/AOSInit";
 export * from "./components/scripts/ClarityHeatmaps";
+export * from "./components/scripts/FacebookPixel";
 export * from "./components/scripts/GoogleAnalytics/GoogleAnalyticsConsent";
 
 // Import CSS styles
